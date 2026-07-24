a12509635f56a602bd574505996b2831cae2ad77
yazdan <mahdi.samimi.doost@gmail.com>
2026-07-24 19:05:52 +0330

    Add Course model and configure Django admin

--- courses/models.py
+++ courses/models.py
@@ -11,3 +11,6 @@ class Course(models.Model):
     student_count = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
