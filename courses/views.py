cd9e345abaa03da3c8ab80b6c0614160003a071a
yazdan <mahdi.samimi.doost@gmail.com>
2026-07-25 11:54:52 +0330

    Add course model, admin, views and basic course list

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Course
 # Create your views here.
+
+
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request,'courses/course_list.html',{'courses':courses})
