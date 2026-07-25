a379d4020a3e3497b52bb75810629254efd689eb
yazdan <mahdi.samimi.doost@gmail.com>
2026-07-25 17:44:18 +0330

    Add instructor model and course relationship

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course , Instructor
 # Register your models here.
 
-admin.site.register(Course)
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title','instructor','price','student_count')
+
+@admin.register(Instructor)
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = ('name','email','phone')
--- courses/models.py
+++ courses/models.py
@@ -3,9 +3,23 @@ from django.db import models
 # Create your models here.
 
 
+class Instructor(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    bio = models.TextField()
+
+
+
+    def __str__(self):
+        return self.name
+
+
+
+
 class Course(models.Model):
     title = models.CharField(max_length=100)
-    teacher = models.CharField(max_length=100)
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
     description = models.TextField()
     price = models.IntegerField()
     student_count = models.IntegerField()
@@ -13,4 +27,5 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+
 
