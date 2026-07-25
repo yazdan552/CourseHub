a98a84f334d47e48d4b4eb834a8d1104c549602c
yazdan <mahdi.samimi.doost@gmail.com>
2026-07-26 00:01:47 +0330

    Add course detail page

--- courses/urls.py
+++ courses/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("",views.course_list,name="course_list",),
-
+    path("<int:course_id>/",views.course_detail,name="course_detail",),
 ]
--- courses/views.py
+++ courses/views.py
@@ -18,3 +18,16 @@ def course_list(request):
         },
     )
 
+def course_detail(request, course_id):
+    course = get_object_or_404(
+        Course,
+        id=course_id,
+    )
+
+    return render(
+        request,
+        "courses/course_detail.html",
+        {
+            "course": course,
+        },
+    )
