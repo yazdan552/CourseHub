2a4b7c5a5be9a9c1e7763e9d8314483a8cabf5f3
yazdan <mahdi.samimi.doost@gmail.com>
2026-07-30 23:20:35 +0330

    Add Category model and ManyToMany relation with Course
    - Create Category model with name, slug, description
    - Add ManyToMany field to Course model
    - Register Category in admin with filter_horizontal
    - Add categories to CourseForm with validation
    - Display categories in course detail page
    - Add slug auto-generation with slugify

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-
-from .models import Course, Instructor, Enrollment
+from .models import Course, Instructor, Enrollment, Category
 
 
 @admin.register(Course)
@@ -9,7 +8,21 @@ class CourseAdmin(admin.ModelAdmin):
         "title",
         "instructor",
         "price",
+        "is_active",
+        "created_at",
     )
+
+    list_filter = (
+        "is_active",
+        "categories",
+    )
+
+    search_fields = (
+        "title",
+        "description",
+    )
+
+    filter_horizontal = ("categories",)  # ویجت بهتر برای ManyToMany
 
 
 @admin.register(Instructor)
@@ -20,20 +33,56 @@ class InstructorAdmin(admin.ModelAdmin):
     )
 
 
-
 @admin.register(Enrollment)
 class EnrollmentAdmin(admin.ModelAdmin):
     list_display = (
         "user",
         "course",
         "enrolled_at",
+        "is_completed",
     )
 
     list_filter = (
         "course",
+        "is_completed",
     )
 
     search_fields = (
         "user__username",
         "course__title",
+    )
+
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "slug",
+        "get_course_count",
+        "created_at",
+    )
+
+    list_filter = (
+        "created_at",
+    )
+
+    search_fields = (
+        "name",
+        "description",
+    )
+
+    prepopulated_fields = {"slug": ("name",)}
+
+
+    readonly_fields = ("created_at",)
+
+    fieldsets = (
+        (None, {
+            "fields": ("name", "slug", "description")
+        }),
+        ("Timestamps", {
+            "fields": ("created_at",),
+            "classes": ("collapse",),
+        }),
     )
